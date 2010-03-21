a0dd3ce2c0f0d0bc36425de112d75f865391df6e
vivek.gupta85 <vivek.gupta85@05a1fac8-0e65-11df-a1b8-81d2ac4bdcbf>
2010-03-21 12:50:51 +0000

    removed the Alter Scripts

--- database/alter_scripts.sql
+++
@@ -1,8 +0,0 @@
-alter table person add column country  VARCHAR(45) null;
-alter table person add column city  VARCHAR(45) null;
-alter table person add column state  VARCHAR(45) null;
-alter table person add column address1  VARCHAR(100) null;
-alter table person add column address2  VARCHAR(100) null;
-alter table person add column locality VARCHAR(100) null;
- 
-
